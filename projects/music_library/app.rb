7f02470fd9c02e196ce5fafad468e054c3924453
Quddus Rahman <qrahman115@gmail.com>
2023-03-29 14:18:05 +0100

    added album repo to app in music_library which is not part of exercise

--- projects/music_library/app.rb
+++ projects/music_library/app.rb
@@ -1,10 +1,16 @@
 require_relative 'lib/database_connection'
 require_relative 'lib/artist_repository'
+require_relative 'lib/album_repository'
 
 DatabaseConnection.connect('music_library') # real data base used here as rspec only uses the test database
 
 artist_repository = ArtistRepository.new
+album_repository = AlbumRepository.new
 
 artist_repository.all.each do |artist|
   puts "#{artist.id} - #{artist.name} - #{artist.genre}"
+end
+
+album_repository.all.each do |album|
+  puts "#{album.id} - #{album.title} - #{album.release_year} - #{album.artist_id}"
 end
